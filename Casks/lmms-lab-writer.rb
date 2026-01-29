34e845b2e904b4793a5992bc6c04850b474eef09
Bo Li <drluodian@gmail.com>
2026-01-29 23:34:03 +0800

    chore: update sha256 for latest build

--- Casks/lmms-lab-writer.rb
+++ Casks/lmms-lab-writer.rb
@@ -1,11 +1,11 @@
 cask "lmms-lab-writer" do
   version "0.1.0"
-  sha256 "d4759353ca6258be2c379bfe5cb9a7b4edac1613cc1abbb9d605349df2688508"
+  sha256 "301a58baf6e5a9cf7ed57b7b736362716471e01d424313be06e9e9585ff5bffb"
 
   url "https://uv96nthsmy3qxwco.public.blob.vercel-storage.com/LMMs-Lab_Writer_#{version}_aarch64.pkg"
   name "LMMs-Lab Writer"
   desc "AI-native LaTeX editor for researchers"
-  homepage "https://latex-writer.vercel.app"
+  homepage "https://writer.lmms-lab.com"
 
   depends_on macos: ">= :catalina"
   depends_on arch: :arm64
